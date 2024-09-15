a8918ae0b8551ba6f26e856650c3a4dab9181494
guo <b22790188@gmail.com>
2024-09-15 11:56:04 +0800

    [feat] create independent resources for each conn
    create independent pty iostream for each connection

--- src/main/java/org/example/gitmazonwebsocketserver/TerminalWebSocketHandler.java
+++ src/main/java/org/example/gitmazonwebsocketserver/TerminalWebSocketHandler.java
@@ -2,6 +2,7 @@ package org.example.gitmazonwebsocketserver;
 
 import com.pty4j.PtyProcess;
 import com.pty4j.PtyProcessBuilder;
+import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.socket.CloseStatus;
@@ -16,24 +17,25 @@ import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+@Log4j2
 public class TerminalWebSocketHandler extends TextWebSocketHandler {
-    private PtyProcess process;
-    private OutputStream outputStream;
-    private ExecutorService outputExecutor;
+//    private PtyProcess process;
+//    private OutputStream outputStream;
+//    private ExecutorService outputExecutor;
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("Connection established");
+
         String[] command = {
             "ssh", "-t", "-o", "LogLevel=QUIET", "-i", "~/.ssh/webserver.pem", "ubuntu@18.182.42.57",
             "sudo docker exec -it pusheventtest bash",
         };
-        process = new PtyProcessBuilder(command).start();
-        outputStream = process.getOutputStream();
+        PtyProcess process = new PtyProcessBuilder(command).start();
         InputStream inputStream = process.getInputStream();
 
-
         // using single thread to send data back to frontend
-        outputExecutor = Executors.newSingleThreadExecutor();
+        ExecutorService outputExecutor = Executors.newSingleThreadExecutor();
         outputExecutor.submit(() -> {
             try {
                 byte[] buffer = new byte[1024];
@@ -45,18 +47,37 @@ public class TerminalWebSocketHandler extends TextWebSocketHandler {
                 e.printStackTrace();
             }
         });
+
+        session.getAttributes().put("process", process);
+        session.getAttributes().put("outputExecutor", outputExecutor);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        String input = message.getPayload();
-        outputStream.write(input.getBytes(StandardCharsets.UTF_8));
-        outputStream.flush();
+        PtyProcess process = (PtyProcess) session.getAttributes().get("process");
+
+        if(process != null) {
+            OutputStream outputStream = process.getOutputStream();
+            String input = message.getPayload();
+            outputStream.write(input.getBytes(StandardCharsets.UTF_8));
+            outputStream.flush();
+        } else {
+            log.error("Pty Process not found for session" + session.getId());
+        }
+
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        process.destroy();
-        outputExecutor.shutdown();
+        PtyProcess process = (PtyProcess) session.getAttributes().get("process");
+        ExecutorService outputExecutor = (ExecutorService) session.getAttributes().get("outputExecutor");
+
+        if(process != null) {
+            process.destroy();
+        }
+
+        if(outputExecutor != null) {
+            outputExecutor.shutdown();
+        }
     }
 }
