4ca21180d09af2c492a42e6a9f3f685b6e34e60c
guo <b22790188@gmail.com>
2024-09-16 11:50:03 +0800

    [feat] get pod info from master node instead of FE

--- src/main/java/org/example/gitmazonwebsocketserver/TerminalWebSocketHandler.java
+++ src/main/java/org/example/gitmazonwebsocketserver/TerminalWebSocketHandler.java
@@ -14,7 +14,12 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -25,69 +30,105 @@ public class TerminalWebSocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         log.info("Connection established");
+
+        //get data from backend
+        String apiUrl = "http://localhost:8082/info?username=guo";
+        String response = fetchPodInfo(apiUrl);
+        Map<String, String> podInfo = new ObjectMapper().readValue(response, Map.class);
+
+        String ip = podInfo.get("podIP");
+        String container = podInfo.get("container");
+
+        log.info("Received Pod Info - IP: " + ip + ", Container: " + container);
+
+        // 建立 SSH 連線
+        String[] command = {
+            "ssh", "-t", "-o", "LogLevel=QUIET", "-i", "~/.ssh/webserver.pem", "ubuntu@" + ip,
+            "sudo docker exec -it " + container + " bash"
+        };
+        PtyProcess process = new PtyProcessBuilder(command).start();
+        InputStream inputStream = process.getInputStream();
+        OutputStream outputStream = process.getOutputStream();
+
+        ExecutorService outputExecutor = Executors.newSingleThreadExecutor();
+        outputExecutor.submit(() -> {
+            try {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    session.sendMessage(new TextMessage(new String(buffer, 0, bytesRead, StandardCharsets.UTF_8)));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        session.getAttributes().put("process", process);
+        session.getAttributes().put("outputExecutor", outputExecutor);
+        session.getAttributes().put("outputStream", outputStream);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        if (!session.getAttributes().containsKey("process")) {
-            // 解析 JSON 訊息
-            String payload = message.getPayload();
-            Map<String, String> initParams = new ObjectMapper().readValue(payload, Map.class);
-
-            String ip = initParams.get("ip");
-            String container = initParams.get("container");
-
-            log.info("Received initialization params - IP: " + ip + ", Container: " + container);
-
-            // create ssh command
-            String[] command = {
-                "ssh", "-t", "-o", "LogLevel=QUIET", "-i", "~/.ssh/webserver.pem", "ubuntu@" + ip,
-                "sudo docker exec -it " + container + " bash"
-            };
-            PtyProcess process = new PtyProcessBuilder(command).start();
-            InputStream inputStream = process.getInputStream();
-
-            // get inputstream data and send it to frontend
-            ExecutorService outputExecutor = Executors.newSingleThreadExecutor();
-            outputExecutor.submit(() -> {
-                try {
-                    byte[] buffer = new byte[1024];
-                    int bytesRead;
-                    while ((bytesRead = inputStream.read(buffer)) != -1) {
-                        session.sendMessage(new TextMessage(new String(buffer, 0, bytesRead, StandardCharsets.UTF_8)));
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-
-            session.getAttributes().put("process", process);
-            session.getAttributes().put("outputExecutor", outputExecutor);
-
-        } else {
-            PtyProcess process = (PtyProcess) session.getAttributes().get("process");
-
-            if (process != null) {
-                OutputStream outputStream = process.getOutputStream();
-                String input = message.getPayload();
-                outputStream.write(input.getBytes(StandardCharsets.UTF_8));
-                outputStream.flush();
-            } else {
-                log.error("Pty Process not found for session" + session.getId());
-            }
-        }
-
-
-//        PtyProcess process = (PtyProcess) session.getAttributes().get("process");
+//        if (!session.getAttributes().containsKey("process")) {
 //
-//        if(process != null) {
-//            OutputStream outputStream = process.getOutputStream();
-//            String input = message.getPayload();
-//            outputStream.write(input.getBytes(StandardCharsets.UTF_8));
-//            outputStream.flush();
+//            // get instance info from master node server
+//            String apiUrl = "http://localhost:8082/info?username=guo";
+//            String response = fetchPodInfo(apiUrl);
+//            Map<String, String> podInfo = new ObjectMapper().readValue(response, Map.class);
+//
+//            String ip = podInfo.get("podIP");
+//            String container = podInfo.get("container");
+//
+//            log.info("Received initialization params - IP: " + ip + ", Container: " + container);
+//
+//            // create ssh command
+//            String[] command = {
+//                "ssh", "-t", "-o", "LogLevel=QUIET", "-i", "~/.ssh/webserver.pem", "ubuntu@" + ip,
+//                "sudo docker exec -it " + container + " bash"
+//            };
+//            PtyProcess process = new PtyProcessBuilder(command).start();
+//            InputStream inputStream = process.getInputStream();
+//
+//            // get inputstream data and send it to frontend
+//            ExecutorService outputExecutor = Executors.newSingleThreadExecutor();
+//            outputExecutor.submit(() -> {
+//                try {
+//                    byte[] buffer = new byte[1024];
+//                    int bytesRead;
+//                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+//                        session.sendMessage(new TextMessage(new String(buffer, 0, bytesRead, StandardCharsets.UTF_8)));
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//
+//            session.getAttributes().put("process", process);
+//            session.getAttributes().put("outputExecutor", outputExecutor);
+//
 //        } else {
-//            log.error("Pty Process not found for session" + session.getId());
+//            PtyProcess process = (PtyProcess) session.getAttributes().get("process");
+//
+//            if (process != null) {
+//                OutputStream outputStream = process.getOutputStream();
+//                String input = message.getPayload();
+//                outputStream.write(input.getBytes(StandardCharsets.UTF_8));
+//                outputStream.flush();
+//            } else {
+//                log.error("Pty Process not found for session" + session.getId());
+//            }
 //        }
+        PtyProcess process = (PtyProcess) session.getAttributes().get("process");
+
+        if (process != null) {
+            OutputStream outputStream = process.getOutputStream();
+            String input = message.getPayload();
+            outputStream.write(input.getBytes(StandardCharsets.UTF_8));
+            outputStream.flush();
+        } else {
+            log.error("Pty Process not found for session" + session.getId());
+        }
     }
 
     @Override
@@ -102,5 +143,15 @@ public class TerminalWebSocketHandler extends TextWebSocketHandler {
         if(outputExecutor != null) {
             outputExecutor.shutdown();
         }
+    }
+
+    private String fetchPodInfo(String apiUrl) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create((apiUrl)))
+            .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
     }
 }
