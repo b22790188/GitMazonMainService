7b6e0f1327efd78e042bc3cbfa7636b1c1d09d44
guo <b22790188@gmail.com>
2024-09-16 11:08:23 +0800

    [feat] dynamic websocket connection in websocket handler

--- src/main/java/org/example/gitmazonwebsocketserver/TerminalWebSocketHandler.java
+++ src/main/java/org/example/gitmazonwebsocketserver/TerminalWebSocketHandler.java
@@ -1,5 +1,6 @@
 package org.example.gitmazonwebsocketserver;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pty4j.PtyProcess;
 import com.pty4j.PtyProcessBuilder;
 import lombok.extern.log4j.Log4j2;
@@ -14,57 +15,79 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Log4j2
 public class TerminalWebSocketHandler extends TextWebSocketHandler {
-//    private PtyProcess process;
-//    private OutputStream outputStream;
-//    private ExecutorService outputExecutor;
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         log.info("Connection established");
-
-        String[] command = {
-            "ssh", "-t", "-o", "LogLevel=QUIET", "-i", "~/.ssh/webserver.pem", "ubuntu@18.182.42.57",
-            "sudo docker exec -it pusheventtest bash",
-        };
-        PtyProcess process = new PtyProcessBuilder(command).start();
-        InputStream inputStream = process.getInputStream();
-
-        // using single thread to send data back to frontend
-        ExecutorService outputExecutor = Executors.newSingleThreadExecutor();
-        outputExecutor.submit(() -> {
-            try {
-                byte[] buffer = new byte[1024];
-                int bytesRead;
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    session.sendMessage(new TextMessage(new String(buffer, 0, bytesRead, StandardCharsets.UTF_8)));
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        session.getAttributes().put("process", process);
-        session.getAttributes().put("outputExecutor", outputExecutor);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        PtyProcess process = (PtyProcess) session.getAttributes().get("process");
+        if (!session.getAttributes().containsKey("process")) {
+            // 解析 JSON 訊息
+            String payload = message.getPayload();
+            Map<String, String> initParams = new ObjectMapper().readValue(payload, Map.class);
 
-        if(process != null) {
-            OutputStream outputStream = process.getOutputStream();
-            String input = message.getPayload();
-            outputStream.write(input.getBytes(StandardCharsets.UTF_8));
-            outputStream.flush();
+            String ip = initParams.get("ip");
+            String container = initParams.get("container");
+
+            log.info("Received initialization params - IP: " + ip + ", Container: " + container);
+
+            // create ssh command
+            String[] command = {
+                "ssh", "-t", "-o", "LogLevel=QUIET", "-i", "~/.ssh/webserver.pem", "ubuntu@" + ip,
+                "sudo docker exec -it " + container + " bash"
+            };
+            PtyProcess process = new PtyProcessBuilder(command).start();
+            InputStream inputStream = process.getInputStream();
+
+            // get inputstream data and send it to frontend
+            ExecutorService outputExecutor = Executors.newSingleThreadExecutor();
+            outputExecutor.submit(() -> {
+                try {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        session.sendMessage(new TextMessage(new String(buffer, 0, bytesRead, StandardCharsets.UTF_8)));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            session.getAttributes().put("process", process);
+            session.getAttributes().put("outputExecutor", outputExecutor);
+
         } else {
-            log.error("Pty Process not found for session" + session.getId());
+            PtyProcess process = (PtyProcess) session.getAttributes().get("process");
+
+            if (process != null) {
+                OutputStream outputStream = process.getOutputStream();
+                String input = message.getPayload();
+                outputStream.write(input.getBytes(StandardCharsets.UTF_8));
+                outputStream.flush();
+            } else {
+                log.error("Pty Process not found for session" + session.getId());
+            }
         }
 
+
+//        PtyProcess process = (PtyProcess) session.getAttributes().get("process");
+//
+//        if(process != null) {
+//            OutputStream outputStream = process.getOutputStream();
+//            String input = message.getPayload();
+//            outputStream.write(input.getBytes(StandardCharsets.UTF_8));
+//            outputStream.flush();
+//        } else {
+//            log.error("Pty Process not found for session" + session.getId());
+//        }
     }
 
     @Override
